6b4d04bc00b3308b9c175e4eb57fe2873d9011e1
Md Ariful Islam <arifcse209@gmail.com>
2023-12-25 12:24:02 +0600

    property binding with image tag

--- Angular_Fundamentals/src/app/app.component.ts
+++ Angular_Fundamentals/src/app/app.component.ts
@@ -7,6 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = "Angular FundaMentals";
-  inputVal = "Hello";
-  isDisabled = false;
+  imgSrc = "/assets/Angular.jpg";
 }
