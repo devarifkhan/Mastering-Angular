90785db11a9ce00782e973f5be5a9efe4fd3eb1b
Md Ariful Islam <arifcse209@gmail.com>
2023-12-25 18:15:54 +0600

    ngSwitch

--- Angular_Directives/src/app/app.component.ts
+++ Angular_Directives/src/app/app.component.ts
@@ -7,15 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular_Directives';
-  show: boolean= false;
-  listItems = [{
-    name: "Arif",
-    age:23
-  }, {
-    name: "Jack",
-    age:25
-    }, {
-    name: "Bella",
-      age:24
-  }]
+  person = {
+    age:20,
+ }
 }
