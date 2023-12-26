29bd8f8b14f724abe77ef87d23ee8a0935237665
Md Ariful Islam <arifcse209@gmail.com>
2023-12-26 11:17:14 +0600

    ngStyle

--- Angular_Directives/src/app/app.component.ts
+++ Angular_Directives/src/app/app.component.ts
@@ -6,9 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular_Directives';
-  colorVal = 'navy';
-  fontSize = '20px';
-  isItalic = 'italic';
+  nm: string = "";
+  em: string = "";
+  emailIsValid: boolean = false;
+  formSubmitted: boolean = false;
+
+
+  checkEmailValidation(em: string): void {
+    if (em.includes('@') && em.includes('.com')) {
+      this.emailIsValid = true;
+
+    } else {
+      this.emailIsValid = false;
+    }
+
+  }
+
+  showMessage() {
+    if (this.nm && this.emailIsValid) {
+      this.formSubmitted = true;
+    } else {
+      this.formSubmitted = false;
+    }
+  }
 
 }
